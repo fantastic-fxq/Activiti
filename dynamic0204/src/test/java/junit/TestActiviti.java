f860618f924b6652df1f0f01866fcde312489230
Fxq <993713343@qq.com>
2017-02-06 17:21:14 +0800

    熟悉Activiti的殷勤和服务组件以及参数

--- dynamic0204/src/test/java/junit/TestActiviti.java
+++ dynamic0204/src/test/java/junit/TestActiviti.java
@@ -10,18 +10,41 @@ public class TestActiviti {
 	 */
 	@Test
 	public void createTable() {
-		ProcessEngineConfiguration processEngineConfiguration=	ProcessEngineConfiguration.createStandaloneProcessEngineConfiguration();
-		
+		ProcessEngineConfiguration processEngineConfiguration = ProcessEngineConfiguration
+				.createStandaloneProcessEngineConfiguration();
+
 		processEngineConfiguration.setJdbcDriver("com.mysql.jdbc.Driver");
-		processEngineConfiguration.setJdbcUrl("jdbc:mysql://localhost:3306/dynamic0204activiti?useUnicode=true&characterEncoding=utf8");
+		processEngineConfiguration
+				.setJdbcUrl("jdbc:mysql://localhost:3306/dynamic0204activiti?useUnicode=true&characterEncoding=utf8");
 		processEngineConfiguration.setJdbcUsername("root");
 		processEngineConfiguration.setJdbcPassword("fxq123");
-		
-		processEngineConfiguration.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
-		
-		//工作流的核心对象，processEnginee对象
-		ProcessEngine processEngine = processEngineConfiguration.buildProcessEngine();
-		System.out.println("processEngine"+processEngine);		
+
+		processEngineConfiguration
+				.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+
+		// 工作流的核心对象，processEnginee对象
+		ProcessEngine processEngine = processEngineConfiguration
+				.buildProcessEngine();
+		System.out.println("processEngine" + processEngine);
+	}
+
+	/**
+	 * 使用xml配置来生成数据库
+	 */
+	@Test
+	public void createTableUseXml() {
+		// 流程引擎的全部配置，读取和解析相应的配置文件，并返回ProcessEngineConfiguration实例
+		// ProcessEngineConfiguration processEngineConfiguration =
+		// ProcessEngineConfiguration//
+		// .createProcessEngineConfigurationFromResource("activiti.cfg.xml");
+		// ProcessEngine processEngine = processEngineConfiguration
+		// .buildProcessEngine();
+
+		ProcessEngine processEngine = ProcessEngineConfiguration//
+				.createProcessEngineConfigurationFromResource("activiti.cfg.xml")//
+				.buildProcessEngine();
+		System.out.println("processEngine" + processEngine);
+
 	}
 
 }
